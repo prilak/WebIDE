bc5998b45db43127919840333f076f85081561e7
Michael Lawrence <mdlawrence@utexas.edu>
2017-11-22 22:01:37 +0000

    passport (user authentication) now works

--- WebIDE/app.js
+++ WebIDE/app.js
@@ -1,13 +1,31 @@
 var fs = require("fs");
 var exec = require("child_process").exec;
 var mysql = require("mysql");
-var bcrypt = require('bcrypt');
+var bCrypt = require('bcrypt-nodejs');
 var bodyParser = require("body-parser");
-
+var passport = require("passport");
 var express = require("express");
+var session = require('express-session');
+var env = require('dotenv').load();
+var exphbs = require('express-handlebars');
 var app = express();
-app.set("view engine", "ejs");
+//app.set("view engine", "ejs");
+
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+// initialize passport
+app.use(session({ secret: 'keyboard cat', resave: true, saveUninitialized:true})); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+
+//For Handlebars
+app.set('views', './views');
+app.engine('hbs', exphbs({
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
+
 
 
 //------------------------------------------
@@ -34,12 +52,24 @@ con.connect(function(err) {
   if (err) throw err;
 });
 
-// some java test code used to create a .java file 
-var java =  "class Main{" + "\n" + 
-            "public static void main(String[] args){" + "\n" + 
-            "System.out.println(\"hello\");" + "\n" +
-            "}" + "\n" +
-            "}";
+//Models
+var models = require("./models");
+
+//load passport strategies
+require('./config/passport/passport.js')(passport, models.user);
+
+//Sync Database
+models.sequelize.sync().then(function() {
+ 
+    console.log('Nice! Database looks fine')
+ 
+}).catch(function(err) {
+ 
+    console.log(err, "Something went wrong with the Database Update!")
+ 
+});
+
+
 //------------------------------------
 // test
 // con.query("SELECT * FROM User", function (err, result, fields) {
@@ -69,64 +99,80 @@ var java =  "class Main{" + "\n" +
 //   console.log('Saved!');
 // }); 
 
-app.get("/", function(req, res){
-    res.render("home");
-});
-app.get("/register", function(req, res) {
-    res.render("register"); 
-});
-app.post("/register", function(req, res) {
-    //console.log(req.body);
-    var username = req.body.username;
-    var password = req.body.password;
-    var saltRounds = 10;
-    bcrypt.genSalt(saltRounds, function(err, salt) {
-        if(err){
-            console.log(err);
-        }
-        bcrypt.hash(password, salt, function(err, hash) {
-            if(err){
-                console.log(err);
-            }
-            var sql = "INSERT INTO User (LastName, FirstName, Username, Password) VALUES (NULL, NULL, " + "\'" + username + "\'" + ", " + "\'" + hash + "\'" + ")";
-            con.query(sql, function(err, result){
-                if(err){
-                    console.log(err);
-                }
-            })
-        // Store hash in your password DB.
-        });
-    });
-    res.redirect("/");
-});
-app.get("/login", function(req, res) {
-    res.render("login");
-});
-app.post("/login", function(req, res){
-    con.query("SELECT Password FROM User WHERE Username = " + "\'" + req.body.username + "\'", function (err, result, fields) {
-        if (err) throw err;
-        //console.log(result + fields);
-        // JSON.parse(result, function(err, hash){
-        //     if(err)throw err;
-        //     res.send(hash);    
-        // });
-        var hash = result[0].Password;
-        console.log(hash);
-        bcrypt.compare(req.body.password, hash, function(err, res) {
-            //res == true
-            if(err) throw err;
-            console.log(res);
-        });
-        res.send(result);
-    });
+//Routes
+var authRoute = require('./routes/auth.js')(app, passport);
+
+
+
+// app.get("/", function(req, res){
+//     res.render("home");
+// });
+// app.get("/register", function(req, res) {
+//     res.render("register"); 
+// });
+// app.post("/register", function(req, res) {
+//     //console.log(req.body);
+//     var username = req.body.username;
+//     var password = req.body.password;
+//     var saltRounds = 10;
+//     bcrypt.genSalt(saltRounds, function(err, salt) {
+//         if(err){
+//             console.log(err);
+//         }
+//         bcrypt.hash(password, salt, function(err, hash) {
+//             if(err){
+//                 console.log(err);
+//             }
+//             var sql = "INSERT INTO User (LastName, FirstName, Username, Password) VALUES (NULL, NULL, " + "\'" + username + "\'" + ", " + "\'" + hash + "\'" + ")";
+//             con.query(sql, function(err, result){
+//                 if(err){
+//                     console.log(err);
+//                 }
+//             });
+            
+//         // Store hash in your password DB.
+//         });
+        
+//     });
+//     passport.authenticate("local")(req, res, function(){
+//         res.redirect("/");
+//     });
+//     res.redirect("/");
+// });
+// app.get("/login", function(req, res) {
+//     res.render("login");
+// });
+// app.get("/secret", isLoggedIn, function(req, res) {
+//     res.render("login"); 
+// });
+// app.post("/login", passport.authenticate("local", {
+//     successRedirect: "/secret",
+//     failureRedirect: "/login"
+//     }), function(req, res){
+//     con.query("SELECT Password FROM User WHERE Username = " + "\'" + req.body.username + "\'", function (err, result, fields) {
+//         if (err) throw err;
+//         //console.log(result + fields);
+//         // JSON.parse(result, function(err, hash){
+//         //     if(err)throw err;
+//         //     res.send(hash);    
+//         // });
+//         var hash = result[0].Password;
+//         console.log(hash);
+//         bcrypt.compare(req.body.password, hash, function(err, res) {
+//             //res == true
+//             if(err) throw err;
+//             console.log(res);
+//         });
+//         res.send(result);
+//     });
     
-});
+// });
 
-app.get("/logout", function(req, res) {
-    req.logout();
-    res.redirect("/");
-});
-
+// app.get("/logout", function(req, res) {
+//     req.logout();
+//     res.redirect("/");
+// });
+console.log(process.env.IP + "   " + process.env.PORT);
 app.listen(process.env.PORT, process.env.IP, function(){
 	console.log("server is running");
 });
